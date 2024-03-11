bc40a076c9888f20e7efa0fdc2aa6b35d153d6bb
Diego Mendes <diego.mendes7@outlook.com>
2024-03-11 17:37:27 -0300

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,29 @@
-import Logo from "../logo/Logo";
-
-document.addEventListener('scroll', function () {
-  const header = document.getElementById('mainHeader');
-  let scrollPosition = window.scrollY;
-
-  if (scrollPosition > 10) {
-    header.style.position = 'relative';
-  } else {
-    header.style.position = 'fixed';
-  }
-})
+import React, { useEffect } from 'react';
+import Logo from '../logo/Logo';
 
 export function Header() {
+  useEffect(() => {
+    function handleScroll() {
+      const header = document.getElementById('mainHeader');
+      let scrollPosition = window.scrollY;
+
+      if (scrollPosition > 10) {
+        header.style.position = 'relative';
+      } else {
+        header.style.position = 'fixed';
+      }
+    }
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header className="fixed w-full flex items-center justify-center pt-8 pr-0 pb-0 xl:pt-8" id="mainHeader">
       <Logo />
     </header>
-  )
+  );
 }
